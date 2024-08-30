24bc74a442e7868e3e29c05020fc2dcba3096ac7
Martim Mourao <martimmourao@gmail.com>
2024-08-30 14:36:58 +0100

    started adding more logic to lexer, implemented complex test

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -6,7 +6,23 @@ import (
 	"interpreter/token"
 )
 
-func TestNextToken(t *testing.T) {
+func validateLexer(t *testing.T, input string, expectedResults []token.Token) {
+	l := New(input)
+
+	for _, expectedResult := range expectedResults {
+		tokenResult := l.NextToken()
+
+		if expectedResult.Type != tokenResult.Type {
+			t.Fatalf("lexer test - tokentype wrong. expected:%q, got %q", expectedResult.Type, tokenResult.Type)
+		}
+
+		if expectedResult.Literal != tokenResult.Literal {
+			t.Fatalf("lexer test - literal wrong. expected:%q, got %q", expectedResult.Literal, tokenResult.Literal)
+		}
+	}
+}
+
+func TestBasicNextToken(t *testing.T) {
 	input := "=+(){},;"
 
 	expectedResults := []token.Token{
@@ -21,17 +37,64 @@ func TestNextToken(t *testing.T) {
 		{Type: token.EOF, Literal: ""},
 	}
 
-	l := New(input)
+	validateLexer(t, input, expectedResults)
+}
 
-	for _, expectedResult := range expectedResults {
-		tokenResult := l.NextToken()
+func TestComplexNextToken(t *testing.T) {
+	input := `
+	let five = 5;
+	let ten = 10;
+	let add = fn(x, y) {
+		x + y;
+	};
+	let result = add(five, ten);
+	`
+	expectedResults := []token.Token{
+		{Type: token.LET, Literal: "let"},
+		{Type: token.IDENT, Literal: "five"},
+		{Type: token.ASSIGN, Literal: "="},
+		{Type: token.INT, Literal: "5"},
+		{Type: token.SEMICOLON, Literal: ";"},
 
-		if expectedResult.Type != tokenResult.Type {
-			t.Fatalf("lexer test - tokentype wrong. expected:%q, got %q", expectedResult.Type, tokenResult.Type)
-		}
+		{Type: token.LET, Literal: "let"},
+		{Type: token.IDENT, Literal: "ten"},
+		{Type: token.ASSIGN, Literal: "="},
+		{Type: token.INT, Literal: "10"},
+		{Type: token.SEMICOLON, Literal: ";"},
 
-		if expectedResult.Literal != tokenResult.Literal {
-			t.Fatalf("lexer test - literal wrong. expected:%q, got %q", expectedResult.Literal, tokenResult.Literal)
-		}
+		{Type: token.LET, Literal: "let"},
+		{Type: token.IDENT, Literal: "add"},
+		{Type: token.ASSIGN, Literal: "="},
+		{Type: token.INT, Literal: "fn"},
+		{Type: token.LPAREN, Literal: "("},
+		{Type: token.IDENT, Literal: "x"},
+		{Type: token.COMMA, Literal: ","},
+		{Type: token.IDENT, Literal: "y"},
+		{Type: token.RPAREN, Literal: ")"},
+		{Type: token.LBRACE, Literal: "{"},
+
+		{Type: token.IDENT, Literal: "x"},
+		{Type: token.PLUS, Literal: "+"},
+		{Type: token.IDENT, Literal: "y"},
+		{Type: token.SEMICOLON, Literal: ";"},
+
+		{Type: token.RBRACE, Literal: "}"},
+		{Type: token.SEMICOLON, Literal: ";"},
+
+		{Type: token.LET, Literal: "let"},
+		{Type: token.IDENT, Literal: "result"},
+		{Type: token.ASSIGN, Literal: "="},
+		{Type: token.IDENT, Literal: "add"},
+		{Type: token.LPAREN, Literal: "("},
+		{Type: token.IDENT, Literal: "five"},
+		{Type: token.COMMA, Literal: ","},
+		{Type: token.IDENT, Literal: "ten"},
+		{Type: token.RPAREN, Literal: ")"},
+		{Type: token.SEMICOLON, Literal: ";"},
+
+		{Type: token.EOF, Literal: ""},
 	}
+
+	validateLexer(t, input, expectedResults)
+
 }

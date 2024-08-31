b5ce44cf5e79811009cc11a6da34b2282b33f402
mdmourao <martimmourao@gmail.com>
2024-08-31 15:43:24 +0100

    Code comments

--- utils/utils.go
+++ utils/utils.go
@@ -5,6 +5,7 @@ func IsLetter(ch byte) bool {
 	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
 }
 
+// we only support int number for now. (12121)
 func IsDigit(ch byte) bool {
 	return '0' <= ch && ch <= '9'
 }
